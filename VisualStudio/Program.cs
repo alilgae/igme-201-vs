e2e5a2d68cbf77b92861ad8cda11d79c56590322
KaidenT <89858394+alilgae@users.noreply.github.com>
2024-09-24 09:05:54 -0400

    Created Project

---
+++ VisualStudio/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace VisualStudio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
